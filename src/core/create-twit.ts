24cadd4127fc2e314444e071e134cfc8ab747bbf
Petro Cardoso <petrolcds@gmail.com>
2024-02-28 19:58:14 -0300

    :recycle: Ajuste

--- src/core/create-twit.ts
+++ src/core/create-twit.ts
@@ -7,9 +7,9 @@ const { NAME_OF_PERIOD, START_DATE, END_DATE } = process.env as Record<
 >
 
 export function createTwitt() {
-  const today = moment()
-  const startDate = moment(START_DATE)
-  const endDate = moment(END_DATE)
+  const today = moment().startOf('day')
+  const startDate = moment(START_DATE).startOf('day')
+  const endDate = moment(END_DATE).startOf('day')
 
   const totalDays = endDate.diff(startDate, 'days') + 1
   const daysFromStart = today.diff(startDate, 'days') + 1
@@ -18,7 +18,7 @@ export function createTwitt() {
   if (daysFromStart <= 0) throw new Error('COUNTDOWN_HAS_NOT_STARTED')
   if (daysFromEnd <= 0) throw new Error('COUNTDOWN_HAS_ENDED')
 
-  const remainingDays = daysFromEnd 
+  const remainingDays = daysFromEnd
   const percentage = Math.round((daysFromStart / totalDays) * 10000) / 100
   const parsedPercentage = percentage > 100 ? 100 : percentage.toFixed(2)
 
